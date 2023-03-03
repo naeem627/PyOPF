0968125090f040a2971f0a9d4b0992e2c475b02f
Naeem Turner Bandele <naeem627@users.noreply.github.com>
2023-03-03 17:09:33 -0500

    refactor: public run_opf added to run.py

--- pyopf/run.py
+++ pyopf/run.py
@@ -1,7 +1,12 @@
+import os
+from pathlib import Path
 from typing import Optional
 
 from data_utilities.data import Data
 from pyopf.OPF import OPF
+from pyopf.parse.parse import parse
+from pyopf.parse.parse_filepaths import parse_filepaths
+from pyopf.util.Log import Log
 
 
 def _run_opf(case: str,
@@ -26,3 +31,31 @@ def _run_opf(case: str,
     opf.save_solution(case_data_raw, filepaths)
 
     return opf.results_summary
+
+
+def run_opf(case: str,
+            dir_cases: str,
+            objective: Optional[str] = "min dev",
+            scenario: Optional[dict] = None):
+    # Get root directory path
+    path_to_root = os.getcwd()
+
+    # # Create Logger # #
+    path_to_log = path_to_root + os.path.sep + 'log' + os.path.sep
+    Path(path_to_log).mkdir(parents=True, exist_ok=True)
+    logger = Log(path_to_log, case)
+
+    # # Create Path to Results # #
+    path_to_results = f"{path_to_root}/results/{case}"
+    if scenario is not None:
+        path_to_results = f"{path_to_root}/results/{case}/{scenario['dir']}"
+    Path(path_to_results).mkdir(parents=True, exist_ok=True)
+
+    filepaths = parse_filepaths(case, path_to_root, dir_cases, scenario, path_to_results, path_to_log)
+
+    # # Parse RAW file and assign grid data to objects # #
+    grid_data, case_data_raw = parse(case, filepaths, logger)
+
+    # # Run OPF # #
+    _opf_results = _run_opf(case, objective, case_data_raw, grid_data, filepaths)
+    return _opf_results
